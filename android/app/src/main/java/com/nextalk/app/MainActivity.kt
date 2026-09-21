c22a4373e2c53a852dd2dfe26e971b76c5eafe11
miz-app-builder <barilaptop1@gmail.com>
2026-09-21 06:32:27 +0600

    Configure native screen restoration

--- android/app/src/main/java/com/nextalk/app/MainActivity.kt
+++ android/app/src/main/java/com/nextalk/app/MainActivity.kt
@@ -4,8 +4,9 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 class MainActivity:ReactActivity(){
  override fun getMainComponentName():String="NexTalk"
  override fun createReactActivityDelegate():ReactActivityDelegate=DefaultReactActivityDelegate(this,mainComponentName,fabricEnabled)
- override fun onCreate(savedInstanceState:Bundle?){super.onCreate(null)}
+ override fun onCreate(savedInstanceState:Bundle?){supportFragmentManager.fragmentFactory=RNScreensFragmentFactory();super.onCreate(null)}
 }
